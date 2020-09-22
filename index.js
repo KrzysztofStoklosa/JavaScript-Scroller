577a1c442ea0a2f96c85d248b4bf7145da1ddc8f
KrzysztofStoklosa <52719465+KrzysztofStoklosa@users.noreply.github.com>
2020-09-22 12:27:47 +0100

    Add scrool feature

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
-  console.log(sections);
+  let currentSectionIndex = 0;
+  let isTrottled = false;
 
   document.addEventListener("mousewheel", function (event) {
-    console.log(event.wheelDelta);
+    if (isTrottled) return;
+    isTrottled = true;
+    setTimeout(function () {
+      isTrottled = false;
+    }, 1000);
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSecion = currentSectionIndex === sections.length - 1;
+      if (isLastSecion) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex = currentSectionIndex + direction;
+    console.log(currentSectionIndex);
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
   });
 });
