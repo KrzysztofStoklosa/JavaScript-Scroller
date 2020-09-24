c7903967a37fc26b6e172251c2fe3e4f03065b7c
KrzysztofStoklosa <52719465+KrzysztofStoklosa@users.noreply.github.com>
2020-09-24 12:38:29 +0100

    Add navigation to the Scroller

--- Scroller.js
+++ Scroller.js
@@ -9,10 +9,8 @@ class Scroller {
       this.isScrolledInToView
     );
     this.currentSectionIndex = Math.max(currentSectionIndex, 0);
-    // this.currentSectionIndex =
-    //  currentSectionIndex < 0 ? 0 : currentSectionIndex;
-    console.log(this.currentSectionIndex);
     this.isTrottled = false;
+    this.drawNavigation();
   }
 
   isScrolledInToView = (el) => {
@@ -48,9 +46,40 @@ class Scroller {
   }
 
   scrollToCurrentSection = () => {
+    this.selectActiveClassItem();
     this.sections[this.currentSectionIndex].scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
+  };
+  drawNavigation = () => {
+    this.navigationContainer = document.createElement("aside");
+    this.navigationContainer.setAttribute("class", "scroller__navigation");
+    const list = document.createElement("ul");
+    this.sections.forEach((section, index) => {
+      const listItem = document.createElement("li");
+
+      listItem.addEventListener("click", () => {
+        this.currentSectionIndex = index;
+        this.scrollToCurrentSection();
+      });
+
+      list.appendChild(listItem);
+    });
+    this.navigationContainer.appendChild(list);
+    document.body.appendChild(this.navigationContainer);
+    this.selectActiveClassItem();
+  };
+  selectActiveClassItem = () => {
+    if (this.navigationContainer) {
+      const navigationItems = this.navigationContainer.querySelectorAll("li");
+      navigationItems.forEach((item, index) => {
+        if (index === this.currentSectionIndex) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+    }
   };
 }
