4dbf3d8e9e85291cadd192621e974b915e6d51f1
KrzysztofStoklosa <52719465+KrzysztofStoklosa@users.noreply.github.com>
2020-10-01 14:32:55 +0100

    Add swiper to detect swipes on touchable devices

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   scroller = new Scroller("#root");
 
   document.addEventListener("wheel", (event) => scroller.listenScroll(event));
+  document.addEventListener("swipeUp", () => scroller.scroll(1));
+  document.addEventListener("swipeDown", () => scroller.scroll(-1));
 });
---
+++ swiper.js
@@ -0,0 +1,53 @@
+class Swiper {
+  constructor() {
+    this.initialX = null;
+    this.initialY = null;
+
+    document.addEventListener("touchstart", (event) => this.startTouch(event));
+    document.addEventListener("touchmove", (event) => this.moveTouch(event));
+
+    // custom events
+    this.events = {
+      swipeUp: new Event("swipeUp"),
+      swipeDown: new Event("swipeDown"),
+      swipeLeft: new Event("swipeLeft"),
+      swipeRight: new Event("swipeRight"),
+    };
+  }
+  startTouch(event) {
+    event.preventDefault();
+    this.initialX = event.touches[0].clientX;
+    this.initialY = event.touches[0].clientY;
+  }
+  moveTouch(event) {
+    if (!this.initialX || !this.initialY) return;
+    const currentX = event.touches[0].clientX;
+    const currentY = event.touches[0].clientY;
+    const diffX = this.initialX - currentX;
+    const diffY = this.initialY - currentY;
+    console.log({ diffX });
+    console.log({ diffY });
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      //we are in left-right swipe
+      if (diffX > 0) {
+        //left swipe
+        document.dispatchEvent(this.events.swipeLeft);
+      } else {
+        //right swipe
+        document.dispatchEvent(this.events.swipeRight);
+      }
+    } else {
+      //up-down swipe
+      if (diffY > 0) {
+        //up swipe
+        document.dispatchEvent(this.events.swipeUp);
+      } else {
+        //down swipe
+        document.dispatchEvent(this.events.swipeDown);
+      }
+    }
+    this.initialX = null;
+    this.initialY = null;
+  }
+}
+new Swiper();
